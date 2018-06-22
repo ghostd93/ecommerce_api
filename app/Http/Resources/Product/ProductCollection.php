6896cbe3c98e6d16a66bec280fe542e5e6fa3088
ghostd93 <p.kuba1993@gmail.com>
2018-06-22 12:37:52 +0200

    Product Collection Modified

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -14,6 +14,20 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return[
+            'data' => $this->collection->transform(function($product){
+                return [
+                    'name' => $product->name,
+                    'total_price' => round((1 - $product->discount / 100) * $product->price / 100, 2),
+                    'rating' => $product->reviews->count() > 0 ?
+                        round($product->reviews->sum('rating') / $product->reviews->count(), 2) :
+                        'No rating yet',
+                    'discount' => $product->discount,
+                    'href' => [
+                        'link' => route('products.show', $product->id)
+                    ]
+                ];
+            })
+        ];
     }
 }
